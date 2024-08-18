9137c82dc25690c60bef12e64773ccd2915dada4
statelinecat <statelinecat@gmail.com>
2024-08-18 11:52:11 +0300

    add dz

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
+# Скачайте любой датасет с сайта https://www.kaggle.com/datasets
+# Загрузите набор данных из CSV-файла в DataFrame.
+# Выведите первые 5 строк данных, чтобы получить представление о структуре данных.
+# Выведите информацию о данных (.info()) и статистическое описание (.describe()).
+# Определите среднюю зарплату (Salary) по городу (City) - используйте файл приложенный к дз - dz.csv
+#
+# В поле сдачи домашнего задания приложите ссылку на репозиторий с кодом.
+
 import pandas as pd
 
-# data = {
-#     'Name': ['Alice', 'Bob', 'Roma', 'Anna'],
-#     'Age': [25, 30, 35, 24],
-#     'City': ['New York', 'Paris', 'London', 'Berlin']
-# }
-# df = pd.DataFrame(data)
-# print(df)
+df = pd.read_csv('1980sClassics.csv')
+print(df.head(5))
 
-df = pd.read_csv('World-happiness-report-2024.csv')
-print(df[df['Country name'] == 'Russia'])
+df.info()
+df.describe()
+
+dfdz = pd.read_csv('dz.csv')
+print(dfdz)
+
+dfdz.dropna(inplace=True)
+
+print(dfdz)
+
+group = dfdz.groupby('City')['Salary'].mean()
+
+print(group)
